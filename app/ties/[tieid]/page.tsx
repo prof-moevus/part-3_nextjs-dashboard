8be09979e4d069947b34d9c5bf78e76495050b4c
Antoine F Moevus <2613844+ant1moevus@users.noreply.github.com>
2024-11-30 17:18:41 -0500

    ✨ Use useParams() instead of an old method

--- app/ties/[tieid]/page.tsx
+++ app/ties/[tieid]/page.tsx
@@ -1,17 +1,25 @@
 'use client'
 
 import { usePathname } from "next/navigation"
+import { useParams } from 'next/navigation';
 
-export default async function TiePage() 
-    {
-        const pathName = usePathname()
-    //const bookID = usePathname
-    const tieID = pathName.split("/").pop();
+/**
+ * 
+ * You should use `useParams` to extract params on a s
+ * client side component
+ */
+export default function TiePage() {
+    const pathName = usePathname()
+    const params = useParams();
+    const { tieid } = params;
+
+    // Altnerative old and brutal way to useParams
+    // const tieID = pathName.split("/").pop();
 
     return (
-    <>
-        <h3>Cravate {tieID}</h3>
-        <p>Path: {pathName}</p>
-    </>
-)
+        <div>
+            <h3>Cravate {tieid}</h3>
+            <p>Path: {pathName}</p>
+        </div>
+    )
 }
